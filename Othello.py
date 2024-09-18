2aed88f827d5bba2fcd51cf9a6f7ef718d9a1041
Natnael Desalegnh <c21ndh@cs.umu.se>
2024-09-18 19:29:00 +0200

    Removed prints

--- Othello.py
+++ Othello.py
@@ -13,15 +13,12 @@ def main(position, time_limit):
     action = OthelloAction(0,0)
     i = 0
     ab_pruning = AlphaBeta()
-    game.print_board()
     while ((time.time() - start_time) < time_limit):
         ab_pruning.set_search_depth(i)
         action = ab_pruning.evaluate(game.clone())
         i += 1
-    print(f"got to depth {i}, best move is:")
     game.make_move(action)
     action.print_move()
-    game.print_board()
 
 
 if __name__ == "__main__":
