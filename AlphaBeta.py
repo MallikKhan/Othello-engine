002a81fe6b27f9951dbfb14ba2207eec71abec2b
Natnael Desalegnh <c21ndh@cs.umu.se>
2024-09-18 19:27:51 +0200

    Cleaned up some unessesary debugging print statements and such.

--- AlphaBeta.py
+++ AlphaBeta.py
@@ -32,30 +32,21 @@ class AlphaBeta(OthelloAlgorithm):
         :return: The move to make as an OthelloAction
     """
     def evaluate(self, othello_position):
-        actions = [OthelloAction(0,0, True)]
+        actions = OthelloAction(0,0, True)
         [max_val, best_action] = self.alpha_beta_algorithm(othello_position, actions, self.search_depth, float('-inf'), float('inf'), othello_position.to_move())
-        #print(f"\nmaxinum:  {max_val}, move:")
-        #ret.print_move()
+
         return best_action
 
     def alpha_beta_algorithm(self, position, action, depth, alpha_value, beta_value, maximizingPlayer):
-        #print(f"depth ={depth}"))
         if depth == 0:
-            #print(f"\Value:  {self.evaluator.evaluate(position)}, move:")
-            #action.print_move()
-            position.print_board()
             return (self.evaluator.evaluate(position), action)
         
         if maximizingPlayer:
-            #print("White to move")
             max_value = float('-inf')
             list_ = position.get_moves()
-            
             a = action
             for item in list_: #each child in the list save the smallest number
                 pos = position.make_move(item)
-                #item.print_move()
-                
                 [value, act] = self.alpha_beta_algorithm(pos, item, depth - 1, alpha_value, beta_value, pos.to_move())
                 if (alpha_value < value):
                     a = act
@@ -63,19 +54,13 @@ class AlphaBeta(OthelloAlgorithm):
                 alpha_value = max(alpha_value, value)
                 if beta_value <= alpha_value:
                     break
-            
-            #a.print_move()
             return (max_value, a)
         else:
-            #print("Black to move")
             max_value = float('inf')
             list_ = position.get_moves()
-            
             a = action
             for item in list_: #each child in the list save the smallest number
                 pos = position.make_move(item)
-                #item.print_move()
-
                 [value, act] = self.alpha_beta_algorithm(pos, item, depth - 1, alpha_value, beta_value, pos.to_move())
                 if (beta_value > value):
                     a = act
@@ -83,7 +68,6 @@ class AlphaBeta(OthelloAlgorithm):
                 beta_value = min(beta_value, value)
                 if beta_value <= alpha_value:
                     break
-            #a.print_move()
             return (max_value, a)
                 
 
